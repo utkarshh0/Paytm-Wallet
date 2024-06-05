a564d50ff4c565272a5f973e188cc029b94a3a52
uTkarsHH001 <utk.arshh.9.2@gmail.com>
2024-06-05 17:22:13 +0530

    Added backend URL

--- frontend/src/baseUrl.js
+++ frontend/src/baseUrl.js
@@ -1,2 +1,2 @@
-export const REACT_APP_BACKEND = "https://paytm-wallet-0nfc.onrender.com/api/v1/"
+export const REACT_APP_BACKEND = "https://paytm-wallet-x178.onrender.com"
 // export const REACT_APP_BACKEND = "http://localhost:3000/api/v1/"
